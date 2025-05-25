55aa7ce3b5f14b294be605df688b1c42e48d9339
Marco <marco@delight.im>
2025-05-25 13:03:52 +0200

    Improve testing environment

--- tests/index.php
+++ tests/index.php
@@ -6,18 +6,25 @@
  * Licensed under the MIT License (https://opensource.org/licenses/MIT)
  */
 
+/*
+ * WARNING:
+ *
+ * Do *not* use these files from the `tests` directory as the foundation
+ * for the usage of this library in your own code. Instead, please follow
+ * the `README.md` file in the root directory of this project.
+ */
+
 // enable error reporting
 \error_reporting(\E_ALL);
 \ini_set('display_errors', 'stdout');
 
-// enable assertions
-\ini_set('assert.active', 1);
-@\ini_set('zend.assertions', 1);
-\ini_set('assert.exception', 1);
-
 \header('Content-type: text/plain; charset=utf-8');
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+function fail($lineNumber) {
+	exit('Error on line ' . $lineNumber);
+}
 
 $db = \Delight\Db\PdoDatabase::fromDsn(
 	new \Delight\Db\PdoDsn(
